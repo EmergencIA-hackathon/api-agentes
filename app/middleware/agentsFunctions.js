3a7b8deaf9e78292f2210858eb52e65c1259b723
Pedro Augusto <pedro.aug2808@gmail.com>
2025-02-25 20:16:22 -0300

    feat: pegando dados de crimes de lesão corporal
    TODO
    - Utilizar enum no atributo tipos_crimes
    - Atualizar agente escrivão para facilitar o entendimento do que o
    relatante quis dizer (Ex: quebrei a mão -> Quebrei os ossos da mão)
    - Fazer com que as chamadas dos agentes especializados sejam async

--- app/middleware/agentsFunctions.js
+++ app/middleware/agentsFunctions.js
@@ -1,27 +1,49 @@
 import { theftDataExtractionAgent } from "../agents/theftAgent.js";
-import { extractorTheftAgentPromptTemplate } from "../agents/promptTemplates.js";
+import {
+    batteryAgentPromptTemplate,
+    extractorTheftAgentPromptTemplate,
+} from "../agents/promptTemplates.js";
+import { batteryDataExtractionAgent } from "../agents/batteryAgent.js";
 
 async function extractTheftData(text) {
-    try{
+    try {
         const prompt = await extractorTheftAgentPromptTemplate.invoke({
-            text: text
-        })
+            text: text,
+        });
 
-        const theftJson = await theftDataExtractionAgent.invoke(prompt)
+        const theftJson = await theftDataExtractionAgent.invoke(prompt);
 
+        console.log("Done extracting theft data.");
         return theftJson;
-    } catch(error){
-        console.error(error);
+    } catch (error) {
+        console.error("Error while extracting theft data:", error);
+        return null;
+    }
+}
+
+async function extractBatteryData(text) {
+    try {
+        const prompt = await batteryAgentPromptTemplate.invoke({
+            text: text,
+        });
+
+        const batteryJson = await batteryDataExtractionAgent.invoke(prompt);
+
+        console.log("Done extracting battery data.");
+        return batteryJson;
+    } catch (error) {
+        console.error("Error while extracting battery data:", error);
         return null;
     }
 }
 
 async function extractSpecializedData(text, crimesArray) {
     const crimesObj = {
-        "Roubo": extractTheftData
-    }
+        "Roubo": extractTheftData,
+        "Lesao Corporal": extractBatteryData,
+    };
 
-    let specializedJsonArr = []
+    let specializedJsonArr = [];
 
     let crimeType;
     for (let i = 0; i < crimesArray.length; i++) {
@@ -31,6 +53,5 @@ async function extractSpecializedData(text, crimesArray) {
 
     return specializedJsonArr;
 }
-
 
 export { extractSpecializedData };
